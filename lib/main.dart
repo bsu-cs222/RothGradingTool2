eb8fe9ee4f5f3ed97adf1198a57a9cce7bc47bbc
BroD-Prod <brodyroth23@gmail.com>
2024-09-09 19:17:15 -0400

    Removed Dead Code

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class GradingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
